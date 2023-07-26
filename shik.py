3ae33b3e86539ede97d5d1ec225910f28b96f50d
Chamith Lakshan <70762572+ClaZieR@users.noreply.github.com>
2023-07-26 14:14:27 +0530

    working with loop -need to add a role

--- shik.py
+++ shik.py
@@ -1,12 +1,17 @@
 import requests
 import pygame
-import os
+import io
 import time
 import speech_recognition as sr
 import openai
 
 # Initialize OpenAI API key
-openai.api_key = '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'
+openai.api_key = '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'
+
+# Initialize Pygame mixer
+pygame.mixer.init()
+
+assistant_role = "female assistant"  # Set the role of the assistant
 
 def convert_speech_to_text():
     recognizer = sr.Recognizer()
@@ -47,40 +52,42 @@ def text_to_voice(text):
     response = requests.post(url, json=data, headers=headers)
 
     if response.status_code == 200:
-        # Remove existing output.mp3 file
-        if os.path.exists("output.mp3"):
-            os.remove("output.mp3")
+        # Convert the response content to audio data
+        audio_data = io.BytesIO(response.content)
 
-        with open("output.mp3", "wb") as f:
-            f.write(response.content)
+        print("Text-to-speech conversion successful.")
 
-        print("Text-to-speech conversion successful. Audio saved as 'output.mp3'")
-
-        pygame.mixer.init()
-        pygame.mixer.music.load("output.mp3")
-        pygame.mixer.music.play()
+        sound = pygame.mixer.Sound(audio_data)
+        sound.play()
 
         # Wait for the audio to finish playing
-        while pygame.mixer.music.get_busy():
+        while pygame.mixer.get_busy():
             time.sleep(0.1)
     else:
         print("Text-to-speech conversion failed. Status Code:", response.status_code)
 
 if __name__ == "__main__":
-    user_text = convert_speech_to_text()
+    print("Listening for commands...")
+    while True:
+        user_text = convert_speech_to_text()
 
-    # Call the OpenAI API and get the response
-    response = openai.Completion.create(
-        engine="text-davinci-002",  # Replace with the engine you want to use
-        prompt=user_text,
-        max_tokens=100  # Adjust the number of tokens for the response
-    )
+        # Check if the user wants to stop
+        if user_text.lower() == "stop":
+            print("Exiting the loop.")
+            break
 
-    if 'choices' in response and len(response['choices']) > 0:
-        ai_response = response['choices'][0]['text']
-        print("AI Response:", ai_response)
+        # Call the OpenAI API and get the response
+        response = openai.Completion.create(
+            engine="text-davinci-002",  # Replace with the engine you want to use
+            prompt=f"{assistant_role}: {user_text}",  # Include the assistant role in the prompt
+            max_tokens=100  # Adjust the number of tokens for the response
+        )
 
-        # Convert AI response to voice
-        text_to_voice(ai_response)
-    else:
-        print("Failed to get AI response.")
+        if 'choices' in response and len(response['choices']) > 0:
+            ai_response = response['choices'][0]['text']
+            print("AI Response:", ai_response)
+
+            # Convert AI response to voice and play it
+            text_to_voice(ai_response)
+        else:
+            print("Failed to get AI response.")
