9523c48fcb84b56407177b3a1ec6f211c8f2d418
Chamith Lakshan <70762572+ClaZieR@users.noreply.github.com>
2023-07-26 15:13:37 +0530

    working prototype

--- elevenlab.py
+++ elevenlab.py
@@ -1,46 +1,22 @@
-import requests
-import pygame
-import os
-import time
 
-def text_to_voice(text):
-    url = "https://api.elevenlabs.io/v1/text-to-speech/21m00Tcm4TlvDq8ikWAM"
+import openai
+assistant_role="User"
 
-    headers = {
-        'xi-api-key': '40cf6900c4850ddc885253c6daf970bb',
-        'Content-Type': 'application/json',
-    }
+openai.api_key = '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'
+chat_history=[]
+while True:
+    user_text = input("User:")
+    prompt = f"{chat_history} {assistant_role}:{user_text}AI:"
+    response = openai.Completion.create(
+        engine="text-davinci-003",  # Replace with the engine you want to use
+        prompt=prompt,
+        max_tokens=100 , # Adjust the number of tokens for the response
+        stop=[f"{assistant_role}:","AI:"]
+    )
 
-    data = {
-        "text": text,
-        "voice_settings": {
-            "stability": 0,
-            "similarity_boost": 0
-        }
-    }
+    print(response)
+    ai_response = response['choices'][0]['text'].replace("\n", "")
 
-    response = requests.post(url, json=data, headers=headers)
-
-    if response.status_code == 200:
-        # Remove existing output.mp3 file
-        if os.path.exists("output.mp3"):
-            os.remove("output.mp3")
-
-        with open("output.mp3", "wb") as f:
-            f.write(response.content)
-
-        print("Text-to-speech conversion successful. Audio saved as 'output.mp3'")
-
-        pygame.mixer.init()
-        pygame.mixer.music.load("output.mp3")
-        pygame.mixer.music.play()
-
-        # Wait for the audio to finish playing
-        while pygame.mixer.music.get_busy():
-            time.sleep(0.1)
-    else:
-        print("Text-to-speech conversion failed. Status Code:", response.status_code)
-
-if __name__ == "__main__":
-    text_to_voice("Hello How can I help you?.")
-
+    chat_history.append(f"{assistant_role}: {user_text}")
+    chat_history.append(f"Assistant:{ai_response}")
+    print("Chat History:", chat_history)
--- shik.py
+++ shik.py
@@ -11,7 +11,11 @@ openai.api_key = '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'
 # Initialize Pygame mixer
 pygame.mixer.init()
 
-assistant_role = "female assistant"  # Set the role of the assistant
+# Set the role of the assistant
+assistant_role = "User"
+
+# List to store the chat history
+chat_history = []
 
 def convert_speech_to_text():
     recognizer = sr.Recognizer()
@@ -37,7 +41,7 @@ def text_to_voice(text):
     url = "https://api.elevenlabs.io/v1/text-to-speech/21m00Tcm4TlvDq8ikWAM"
 
     headers = {
-        'xi-api-key': '40cf6900c4850ddc885253c6daf970bb',
+        'xi-api-key': '5383faaead6cd34a85f7b748cea715f8',
         'Content-Type': 'application/json',
     }
 
@@ -76,11 +80,13 @@ if __name__ == "__main__":
             print("Exiting the loop.")
             break
 
-        # Call the OpenAI API and get the response
+        # Combine the chat history and current input
+        prompt = f"{chat_history} {assistant_role}:{user_text}AI:"
         response = openai.Completion.create(
-            engine="text-davinci-002",  # Replace with the engine you want to use
-            prompt=f"{assistant_role}: {user_text}",  # Include the assistant role in the prompt
-            max_tokens=100  # Adjust the number of tokens for the response
+        engine="text-davinci-003",  # Replace with the engine you want to use
+        prompt=prompt,
+        max_tokens=100 , # Adjust the number of tokens for the response
+        stop=[f"{assistant_role}:","AI:"]  # Adjust the number of tokens for the response
         )
 
         if 'choices' in response and len(response['choices']) > 0:
@@ -89,5 +95,11 @@ if __name__ == "__main__":
 
             # Convert AI response to voice and play it
             text_to_voice(ai_response)
+
+            # Add the user input and AI response to the chat history
+            chat_history.append(f"{assistant_role}: {user_text}")
+            chat_history.append(f"Assistant:{ai_response}")
+            print("Chat History:", chat_history)
+
         else:
             print("Failed to get AI response.")
